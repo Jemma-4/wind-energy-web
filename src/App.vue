771558040c4efaa60b8f5171f6cb03d127675201
Jemma-4 <1939336810@qq.com>
2023-07-08 00:50:52 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <dv-full-screen-container>
+      <router-view />
+    </dv-full-screen-container>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  height: 100%;
+  width: 100%;
+  background: yellow;
+  font:16px !important;
+}
+#dv-full-screen-container {
+  background-image: url("./assets/bg.png");
+  background-size: 100% 100%;
+  width:100% !important; 
+  height:100% !important;
+  background-attachment:fixed;
+  margin: 0px;
+  padding:0px;
+  box-shadow: 0 0 3px blue;
+  display: flex;
+  flex-direction: column;
+  transform:scale(1) !important;
+  
+}
+element.style {
+    width:100%;
+    height:100%
+}
+
+>>>div.el-picker-panel{
+  background: red !important;
+}
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import * as echarts from 'echarts'
+import dataV from '@jiaminghi/data-view'
+import './assets/dark.css'
+
+Vue.config.productionTip = false
+Vue.prototype.$echarts = echarts
+Vue.use(ElementUI)
+Vue.use(dataV)
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
